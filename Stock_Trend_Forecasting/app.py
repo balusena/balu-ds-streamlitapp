f662c92093407d2f642ec8406cd042f39441cf82
balusena <57207778+balusena@users.noreply.github.com>
2023-01-28 19:44:49 +0000

    Update app.py

--- Stock_Trend_Forecasting/app.py
+++ Stock_Trend_Forecasting/app.py
@@ -52,6 +52,7 @@ plot_raw_data()
 
 # Predict forecast with Prophet.
 df_train = data[['Date', 'Close']]
+df['Date'] = df['Date'].dt.date
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
