4306e603ca3c9aaac7ca9440d0e47d4b1a99a4b6
balusena <57207778+balusena@users.noreply.github.com>
2024-06-14 21:09:22 +0100

    Update app.py

--- Stock_Trend_Forecasting/app.py
+++ Stock_Trend_Forecasting/app.py
@@ -1,45 +1,49 @@
-# pip install streamlit prophet yfinance plotly
-
 import os
-os.environ['TZ'] = 'UTC'
-
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import pandas as pd
 
+# Set the time zone environment variable
+os.environ['TZ'] = 'UTC'
 
+# Define constants
 START = "2020-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
+# Streamlit app title
 st.title('Stock Trend Forecasting')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'META')
+# Dropdown for selecting stock ticker
+stocks = ('GOOG', 'AAPL', 'MSFT', 'META', 'AMZN')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
+# Slider for selecting number of years to predict
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-
-@st.cache
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
+    # Ensure the index is a DatetimeIndex
+    if not isinstance(data['Date'], pd.DatetimeIndex):
+        data['Date'] = pd.to_datetime(data['Date'])
     return data
 
-
+# Load data
 data_load_state = st.text('Loading data...')
 data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
 
+# Display raw data
 st.subheader('Raw data')
 st.write(data.tail())
 
-
-# Plot raw data
+# Plot raw data function
 def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
@@ -47,27 +51,31 @@ def plot_raw_data():
     fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
-
+# Plot raw data
 plot_raw_data()
 
-# Predict forecast with Prophet.
-df_train = data[['Date', 'Close']]
-df_train['Date'] = df_train['Date'].dt.date
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+# Prepare data for forecasting
+df_train = data[['Date', 'Close']].rename(columns={"Date": "ds", "Close": "y"})
+df_train['ds'] = df_train['ds'].dt.date
 
+# Train the Prophet model
 m = Prophet()
 m.fit(df_train)
+
+# Create future dataframe and make predictions
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
-# Show and plot forecast
+# Display forecast data
 st.subheader('Forecast data')
 st.write(forecast.tail())
 
+# Plot forecast
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
+# Plot forecast components
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
